514e808002cbbc031e45738582e3be1dd16ed370
7wyd2284p7-afk <7wyd2284p7-afk@users.noreply.github.com>
2026-09-16 13:26:07 +0800

    Add lessons and improve translation comparison
    # Conflicts:
    #	README.md
    #	src/lessons.js

--- src/lessons.js
+++ src/lessons.js
@@ -107,4 +107,82 @@ window.RETRANSLATE_LESSONS = [
     original:
       "On Wednesday evening, we went to the Town Hall. It was the last day of the year and a large crowd of people had gathered under the Town Hall clock. It would strike twelve in twenty minutes’ time. Fifteen minutes passed and then, at five to twelve, the clock stopped. The big minute hand did not move. We waited and waited, but nothing happened. Suddenly someone shouted, ‘It’s two minutes past twelve! The clock has stopped!’ I looked at my watch. It was true. The big clock refused to welcome the New Year. At that moment, everybody began to laugh and sing.",
   },
+  {
+    id: "nce2-010",
+    book: "新概念英语 2",
+    number: 10,
+    total: 96,
+    title: "Not for jazz",
+    titleCn: "不适于演奏爵士乐",
+    chinese:
+      "我家有件古乐器，被称作古钢琴，是1681年德国造的。我们的这架古钢琴存放在起居室里。我们家有这件乐器已经很久了，它是我祖父在很多年以前买的。可它最近被一个客人弄坏了，因为她用它来弹奏爵士乐。她在击琴键时用力过猛，损坏了两根琴弦。我父亲大为吃惊，不许我们再动它。父亲的一个朋友正在修理这件乐器。",
+    original:
+      "We have an old musical instrument. It is called a clavichord. It was made in Germany in 1681. Our clavichord is kept in the living room. It has belonged to our family for a long time. The instrument was bought by my grandfather many years ago. Recently it was damaged by a visitor. She tried to play jazz on it! She struck the keys too hard and two of the strings were broken. My father was shocked. Now we are not allowed to touch it. It is being repaired by a friend of my father’s.",
+  },
+  {
+    id: "nce2-011",
+    book: "新概念英语 2",
+    number: 11,
+    total: 96,
+    title: "One good turn deserves another",
+    titleCn: "礼尚往来",
+    chinese:
+      "我正在一家饭馆吃饭，托尼·斯蒂尔走了进来。托尼曾在一家律师事务所工作，而现在正在一家银行上班。他的薪水很高，但他却总是向朋友借钱，并且从来不还。托尼看见了我，就走过来和我坐到一张桌子前。他从未向我借过钱。当他吃饭时，我提出向他借20英镑。令我惊奇的是，他立刻把钱给了我。“我还从未向你借过钱，”托尼说道，“所以现在你可以替我付饭钱了！”",
+    original:
+      "I was having dinner at a restaurant when Tony Steele came in. Tony worked in a lawyer’s office years ago, but he is now working at a bank. He gets a good salary, but he always borrows money from his friends and never pays it back. Tony saw me and came and sat at the same table. He has never borrowed money from me. While he was eating, I asked him to lend me twenty pounds. To my surprise, he gave me the money immediately. “I have never borrowed any money from you,” Tony said, “so now you can pay for my dinner!”",
+  },
+  {
+    id: "nce2-012",
+    book: "新概念英语 2",
+    number: 12,
+    total: 96,
+    title: "Goodbye and good luck",
+    titleCn: "再见，一路顺风",
+    chinese:
+      "我们的邻居查尔斯·艾利森船长明天就要从朴次茅斯起航了。明天一大早我们将在码头为他送行。他将乘坐他的“涛波赛”号小艇。“涛波赛”号是艘有名的小艇，它已经多次横渡大西洋。艾利森船长将于8点钟起航，因此我们有充裕的时间。我们将参观他的船，然后和他告别。他要离开两个月，我们真为他感到自豪。他将参加一次重大的横渡大西洋的比赛。",
+    original:
+      "Our neighbour, Captain Charles Alison, will sail from Portsmouth tomorrow. We’ll meet him at the harbour early in the morning. He will be in his small boat, Topsail. Topsail is a famous little boat. It has sailed across the Atlantic many times. Captain Alison will set out at eight o’clock, so we’ll have plenty of time. We’ll see his boat and then we’ll say goodbye to him. He will be away for two months. We are very proud of him. He will take part in an important race across the Atlantic.",
+  },
+  {
+    id: "nce2-013",
+    book: "新概念英语 2",
+    number: 13,
+    total: 96,
+    title: "The Greenwood Boys",
+    titleCn: "绿林少年",
+    chinese:
+      "“绿林少年”是一个流行歌曲演唱团。目前他们正在全国各地巡回演出，明天就要到达此地。他们将乘火车来，镇上的大部分青年人将到车站迎接他们。明晚他们将在工人俱乐部演出。“绿林少年”准备在此逗留5天。在此期间，他们将演出5场。同往常一样，警察的日子将不好过，他们将设法维持秩序。每逢这种场合，情况都是这样。",
+    original:
+      "The Greenwood Boys are a group of pop singers. At present, they are visiting all parts of the country. They will be arriving here tomorrow. They will be coming by train and most of the young people in the town will be meeting them at the station. Tomorrow evening they will be singing at the Workers’ Club. The Greenwood Boys will be staying for five days. During this time, they will give five performances. As usual, the police will have a difficult time. They will be trying to keep order. It is always the same on these occasions.",
+  },
+  {
+    id: "nce2-014",
+    book: "新概念英语 2",
+    number: 14,
+    total: 96,
+    title: "Do you speak English?",
+    titleCn: "你会讲英语吗？",
+    chinese:
+      "去年我有过一次有趣的经历。在离开法国南部的一个小村庄后，我继续驶往下一个城镇。途中，一个青年人向我招手。我把车停下，他向我提出要求搭车。他一上车，我就用法语向他问早上好，他也同样用法语回答我。除了个别几个单词外，我根本不会法语。旅途中我们谁也没讲话。就要到达那个镇时，那青年突然开了口，慢慢地说道：“你会讲英语吗？”我很快了解到，他自己就是个英国人！",
+    original:
+      "I had an amusing experience last year. After I had left a small village in the south of France, I drove on to the next town. On the way, a young man waved to me. I stopped and he asked me for a lift. As soon as he had got into the car, I said good morning to him in French and he replied in the same language. Apart from a few words, I do not know any French at all. Neither of us spoke during the journey. I had nearly reached the town, when the young man suddenly said, very slowly, “Do you speak English?” As I soon learnt, he was English himself!",
+  },
+  {
+    id: "nce2-015",
+    book: "新概念英语 2",
+    number: 15,
+    total: 96,
+    title: "Good news",
+    titleCn: "佳音",
+    chinese:
+      `秘书告诉我说哈姆斯沃思先生要见我。我走进他的办公室，感到非常紧张。我进去的时候，他连头也没抬。待我坐下后，他说生意非常不景气。他还告诉我，公司支付不起这么庞大的工资开支，有20个人已经离去。我知道这次该轮到我了。
+“哈姆斯沃思先生，”我无力地说。
+“不要打断我的话，”他说。
+然后他微笑了一下告诉我说，我每年将得到1,000英镑的额外收入。`,
+    original:
+      `The secretary told me that Mr. Harmsworth would see me. I felt very nervous when I went into his office. He did not look up from his desk when I entered. After I had sat down, he said that business was very bad. He told me that the firm could not afford to pay such large salaries. Twenty people had already left. I knew that my turn had come.
+“Mr. Harmsworth,” I said in a weak voice.
+“Don’t interrupt,” he said.
+Then he smiled and told me I would receive an extra thousand pounds a year!`,
+  },
 ];
